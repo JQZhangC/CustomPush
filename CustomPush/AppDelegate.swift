3c4b3a78bd7e928cdcbf64cded8ca682c089459a
zhangjiaqi <zhangjiaqi@chinahddz.com>
2023-12-11 17:14:18 +0800

    no message

--- CustomPush/AppDelegate.swift
+++ CustomPush/AppDelegate.swift
@@ -6,31 +6,106 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        replyPushNotificationAuthorization(application)
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    private func replyPushNotificationAuthorization(_ application: UIApplication) {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.getNotificationSettings { (setting) in
+            print(setting.authorizationStatus)
+            if setting.authorizationStatus == .notDetermined {
+                center.requestAuthorization(options: [.badge,.sound,.alert]) { (result, error) in
+                    if(result){
+                        if !(error != nil){
+                            // 注册成功
+                            DispatchQueue.main.async {
+                                application.registerForRemoteNotifications()
+                            }
+                        }
+                    } else{
+                        //用户不允许推送
+                    }
+                }
+            } else if (setting.authorizationStatus == .denied){
+                // 申请用户权限被拒
+            } else if (setting.authorizationStatus == .authorized){
+                // 用户已授权（再次获取dt）
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            } else {
+                // 未知错误
+            }
+        }
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+
+        let tokenString = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+         // 上报deviceToken
+        print("deviceToken===========\(tokenString)");
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("[DeviceToken Error]:\(error.localizedDescription)\n");
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        print("didReceiveRemoteNotification===========\n");
+        print(userInfo)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print("didReceiveRemoteNotification===========\n");
+        print(userInfo)
+        completionHandler(.newData)
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("didReceive===========\n");
+        completionHandler()
+    }
+    
+    // MARK: - App处于前台接收到通知
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("willPresent===========\n");
+        if #available(iOS 14.0, *) {
+            completionHandler([.sound, .badge, .list])
+        } else {
+            completionHandler([.sound, .badge, .alert])
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, openSettingsFor notification: UNNotification?) {
+        print("openSettingsFor===========\n");
+    }
+    
+}
